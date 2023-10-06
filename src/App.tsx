136d1bc74480daf053c85a90e26c46676e6d3b34
naetharu <naetharu@gmail.com>
2023-10-06 16:01:48 +0100

    add spartan and antonio fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App flex justify-center bg-green text-lightBlue">
-      FEM CREATE REACT APP
+    <div className="App flex flex-col justify-center items-center bg-green text-black font-spartan text-lg h-screen">
+      <p className="font-spartan">Spartan Normal</p>
+      <p className="font-spartan font-bold">Spartan Bold</p>
+      <p className="font-antonio">Antonio Normal</p>
     </div>
   );
 }
